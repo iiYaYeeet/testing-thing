dad8a22e0a293684dff448e0e368d74696f9eda3
MSI\CJRow <cjrowland05@gmail.com>
2025-02-09 20:53:32 -0500

    Recommit, Should work now

---
+++ testland/Assets/Rockettest/script/expansioncontroller.cs
@@ -0,0 +1,250 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class expansioncontroller : MonoBehaviour
+{
+    [Header("------------------Player Driven Variables------------------")]
+    [Space(2)]
+    [Tooltip("0-1, higher=more clean")] [Range(0,1)] public float mixtureRatio;
+    [Tooltip("10-400 Bar")] public float chamberPressure;
+    [Tooltip("todo")] public float throatradius;
+    [Tooltip("todo")] public float exitradius;
+    [Tooltip("Fuel Combination")] [SerializeField] fuelComp FC = new fuelComp();
+    [Tooltip("Engine Cycle")][SerializeField] engineCycle EC = new engineCycle();
+    [Tooltip("70-120 during normal runs")] [Range(70,120)] public float throttle;
+    [Space(2)]
+    [Header("-----------------'Engine' Driven Variables------------------")]
+    [Space(2)]
+    [Tooltip("0-1, higher=more clean")] [Range(0,1)] [SerializeField] private float stoichemetry;
+    [Tooltip("1000-6000 km/s")] [SerializeField] private float exhaustVelocity;
+    [Tooltip("Exhaust products")] [SerializeField] exhaust ex = new exhaust();
+    [Tooltip("0.8-4 atmo")] [SerializeField] private float exhaustPressure;
+    [Tooltip("0.8-4 atmo")] [SerializeField] private float exhaustTemperature;
+    [Tooltip("todo")] [SerializeField] private float flowRate;
+    [Tooltip("todo")] [SerializeField] private float exitArea;
+    [Tooltip("todo")] [SerializeField] private float exitRatio;
+    [Tooltip("stands at 1 atmo always")] [SerializeField] private float atmosphericPressure=1;
+    [Tooltip("stands at 9.8ms always always")] [SerializeField] private float gravity=9.8f;
+    [Tooltip("500-5000 degrees Kelvin")] [SerializeField] private float chamberTemperature = 3670;
+    [Tooltip("MN")] public float thrust;
+    [Tooltip("Seconds")] public float ISP;
+    [Space(2)]
+    [Header("-------------------Displayed Variables---------------------")]
+    [Space(2)]
+    [Tooltip("MN")] public float displayedthrust;
+    [Tooltip("s")] public float displayedisp;
+    [Tooltip("60-140")] public float trueThrottle;
+    [Tooltip("60-140")] public float throttlevalue;
+
+
+    [Space(2)]
+    [Header("-------------------Other---------------------")]
+    [Space(2)]
+    [SerializeField] private float pressure;
+    public Animator anim;
+    public ParticleSystem ps, ps2,mach;
+    public Transform holder;
+    [SerializeField] private Gradient uncleansl, uncleanalt, stoichometricsl, stoichometricalt, sealevel, altitude, result, uncleanpsl, uncleanpalt, stoichometricpsl,stoichometricpalt, sealevelp, altitudep, resultp;
+    [SerializeField] private AnimationCurve sealevelc, altitudec, resultc;
+    [SerializeField] private List<ParticleSystem> pses;
+    
+    enum fuelComp
+    {
+        Kerolox,
+        Methalox,
+        Hydrolox,
+        Hypergolic
+    }
+
+    enum exhaust
+    {
+        Sooty,
+        Water,
+        Methane,
+        Cancerjuice
+    }
+    enum engineCycle
+    {
+        Open,
+        ClosedFuelRich,
+        ClosedOxRich,
+        FullFlowStaged,
+        PressureFed
+    }
+
+
+    public void Start()
+    {
+        resultc.CopyFrom(sealevelc);
+    }
+
+    public void FixedUpdate()
+    {
+        #region Nozzle Calculations
+        exitArea = Mathf.PI*(exitradius*Mathf.Exp(2));
+        exitRatio = exitradius/throatradius;
+        //chamberTemperature = stoichemetry;
+        #endregion
+        #region exhaust calculations
+        flowRate = chamberPressure * (exitArea / exitRatio);
+        exhaustPressure = (chamberPressure/100) / exitRatio;
+        exhaustTemperature = chamberTemperature * exhaustPressure;
+        exhaustVelocity = flowRate / (atmosphericPressure*exhaustPressure);
+        #endregion
+        #region Thrust and ISP calc
+        thrust = flowRate / exhaustVelocity*exhaustPressure;
+        ISP = exhaustVelocity / gravity;
+        #endregion
+
+        #region visexhaust calculations
+
+        stoichemetry = mixtureRatio;
+        pressure = Mathf.Sqrt(exhaustPressure / atmosphericPressure);
+
+        #endregion
+        sealevel = gradientconvert.Lerp(uncleansl, stoichometricsl, stoichemetry);
+        altitude = gradientconvert.Lerp(uncleanalt, stoichometricalt, stoichemetry);
+        result = gradientconvert.Lerp(sealevel, altitude, pressure);
+        sealevelp = gradientconvert.Lerp(uncleanpsl, stoichometricpsl, stoichemetry);
+        altitudep = gradientconvert.Lerp(uncleanpalt, stoichometricpalt, stoichemetry);
+        resultp = gradientconvert.Lerp(sealevelp, altitudep, pressure);
+
+        var sz = ps.sizeOverLifetime;
+        var cz = ps.colorOverLifetime;
+        var czp = ps2.colorOverLifetime;
+        var mainModule = ps.main;
+        var machmain= mach.main;
+        holder.transform.localScale = new Vector3(1, Mathf.Lerp(1, 0.1f, pressure*throttlevalue), 1);
+        machmain.startColor = Color.Lerp(Color.white, new Color(255, 255, 255, 0),pressure);
+        mainModule.startSize = Mathf.SmoothStep(4, 40, pressure);
+        mainModule.startLifetime = Mathf.SmoothStep(0.2f, 0.4f, pressure);
+        mainModule.startSpeed =Mathf.SmoothStep(120*throttlevalue, 160*throttlevalue, pressure);
+        cz.color = new ParticleSystem.MinMaxGradient(result);
+        czp.color = new ParticleSystem.MinMaxGradient(resultp);
+        sz.size = new ParticleSystem.MinMaxCurve(1f, resultc);
+        var mainpar = ps2.main;
+        mainpar.startSize = Mathf.SmoothStep(20, 70, pressure);
+        mainpar.startSpeed =Mathf.SmoothStep(250*throttlevalue, 320*throttlevalue, pressure);
+        mainpar.startColor = new ParticleSystem.MinMaxGradient(new Color(1, 1, 1, Mathf.Lerp(0.7f, 0.001f, pressure)));
+        foreach (ParticleSystem ps in pses)
+        {
+            var sz1 = ps.sizeOverLifetime;
+            var cz1 = ps.colorOverLifetime;
+            var mainModule1 = ps.main;
+            cz1.color = new ParticleSystem.MinMaxGradient(result);
+            sz1.size = new ParticleSystem.MinMaxCurve(1.2f, resultc);
+            mainModule1.startSize = Mathf.SmoothStep(4, 30*pses.IndexOf(ps), pressure);
+            mainModule1.startLifetime = Mathf.SmoothStep(0.2f, 0.45f, pressure);
+            mainModule1.startSpeed =Mathf.SmoothStep(120*throttlevalue, 160*throttlevalue*pses.IndexOf(ps), pressure);
+        }
+    }
+    public void Update()
+    {
+        throttle = Mathf.Clamp(throttle, 70, 120);
+        throttlevalue = throttle/100;
+        if (Input.GetKey(KeyCode.E))
+        {
+            anim.enabled = true;
+            anim.Play("Startup");
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            anim.enabled = true;
+            anim.Play("Shutdown");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            throttle += 1f;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            throttle -= 1f;
+        }
+        for (int i = 0; i < sealevelc.length; i++)
+        {
+            resultc.MoveKey(i, new Keyframe(altitudec[i].time, Mathf.SmoothStep(sealevelc[i].value,altitudec[i].value, pressure)));
+            resultc.SmoothTangents(i,0.2f);
+        }
+    }
+
+    public void animshut()
+    {
+        anim.enabled = false;
+    }
+     public static class gradientconvert
+    {
+        public static UnityEngine.Gradient Lerp(UnityEngine.Gradient a, UnityEngine.Gradient b, float t)
+        {
+            return Lerp(a, b, t, false, false);
+        }
+
+        public static UnityEngine.Gradient LerpNoAlpha(UnityEngine.Gradient a, UnityEngine.Gradient b, float t)
+        {
+            return Lerp(a, b, t, true, false);
+        }
+
+        public static UnityEngine.Gradient LerpNoColor(UnityEngine.Gradient a, UnityEngine.Gradient b, float t)
+        {
+            return Lerp(a, b, t, false, true);
+        }
+
+        static UnityEngine.Gradient Lerp(UnityEngine.Gradient a, UnityEngine.Gradient b, float t, bool noAlpha, bool noColor)
+        {
+            //list of all the unique key times
+            var keysTimes = new List<float>();
+
+            if (!noColor)
+            {
+                for (int i = 0; i < a.colorKeys.Length; i++)
+                {
+                    float k = a.colorKeys[i].time;
+                    if (!keysTimes.Contains(k))
+                        keysTimes.Add(k);
+                }
+
+                for (int i = 0; i < b.colorKeys.Length; i++)
+                {
+                    float k = b.colorKeys[i].time;
+                    if (!keysTimes.Contains(k))
+                        keysTimes.Add(k);
+                }
+            }
+
+            if (!noAlpha)
+            {
+                for (int i = 0; i < a.alphaKeys.Length; i++)
+                {
+                    float k = a.alphaKeys[i].time;
+                    if (!keysTimes.Contains(k))
+                        keysTimes.Add(k);
+                }
+
+                for (int i = 0; i < b.alphaKeys.Length; i++)
+                {
+                    float k = b.alphaKeys[i].time;
+                    if (!keysTimes.Contains(k))
+                        keysTimes.Add(k);
+                }
+            }
+
+            GradientColorKey[] clrs = new GradientColorKey[keysTimes.Count];
+            GradientAlphaKey[] alphas = new GradientAlphaKey[keysTimes.Count];
+
+            //Pick colors of both gradients at key times and lerp them
+            for (int i = 0; i < keysTimes.Count; i++)
+            {
+                float key = keysTimes[i];
+                var clr = Color.LerpUnclamped(a.Evaluate(key), b.Evaluate(key), t);
+                clrs[i] = new GradientColorKey(clr, key);
+                alphas[i] = new GradientAlphaKey(clr.a, key);
+            }
+
+            var g = new UnityEngine.Gradient();
+            g.SetKeys(clrs, alphas);
+
+            return g;
+        }
+    }
+}
